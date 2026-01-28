28ccd40cb7bb55feb1caad86099078654c1e8dcb
Andy <aayre@esacademy.com>
2026-01-27 19:49:24 -0700

    Added binary files. Copied ControllerV2 to Slave, no changes yet.

---
+++ Firmware/Slave/src/UDPTransfer.cpp
@@ -0,0 +1,129 @@
+/*
+ * UDPTransfer.cpp
+ * 
+ * UDP wrapper for reliable packet-based communication.
+ * Provides the same interface as SerialTransfer but for UDP.
+ */
+
+#include "UDPTransfer.h"
+
+UDPTransfer::UDPTransfer()
+    : _udp(nullptr)
+    , _remoteIP(0, 0, 0, 0)
+    , _remotePort(0)
+    , _bytesRead(0)
+    , _status(PACKET_NO_DATA)
+{
+    memset(_udpBuffer, 0, sizeof(_udpBuffer));
+}
+
+void UDPTransfer::begin(EthernetUDP& udp, uint32_t timeout)
+{
+    _udp = &udp;
+    packet.begin(timeout);
+}
+
+void UDPTransfer::setRemote(IPAddress ip, uint16_t port)
+{
+    _remoteIP = ip;
+    _remotePort = port;
+}
+
+uint8_t UDPTransfer::sendData(uint16_t messageLen, uint8_t packetId)
+{
+    if (_udp == nullptr)
+    {
+        _status = PACKET_PAYLOAD_ERROR;
+        return 0;
+    }
+    
+    // Construct the packet
+    uint8_t numBytesIncl = packet.constructPacket(messageLen, packetId);
+    
+    // Calculate total packet size
+    uint16_t totalSize = PACKET_PREAMBLE_SIZE + numBytesIncl + PACKET_POSTAMBLE_SIZE;
+    
+    // Build the complete packet in the UDP buffer
+    uint16_t idx = 0;
+    
+    // Copy preamble
+    uint8_t* preamble = packet.getPreamble();
+    for (uint8_t i = 0; i < PACKET_PREAMBLE_SIZE; i++)
+    {
+        _udpBuffer[idx++] = preamble[i];
+    }
+    
+    // Copy payload
+    uint8_t* txBuff = packet.getTxBuff();
+    for (uint8_t i = 0; i < numBytesIncl; i++)
+    {
+        _udpBuffer[idx++] = txBuff[i];
+    }
+    
+    // Copy postamble
+    uint8_t* postamble = packet.getPostamble();
+    for (uint8_t i = 0; i < PACKET_POSTAMBLE_SIZE; i++)
+    {
+        _udpBuffer[idx++] = postamble[i];
+    }
+    
+    // Send the packet
+    _udp->beginPacket(_remoteIP, _remotePort);
+    _udp->write(_udpBuffer, totalSize);
+    _udp->endPacket();
+    
+    return numBytesIncl;
+}
+
+uint8_t UDPTransfer::available()
+{
+    if (_udp == nullptr)
+    {
+        _status = PACKET_PAYLOAD_ERROR;
+        return 0;
+    }
+    
+    // Check for incoming UDP packet
+    int packetSize = _udp->parsePacket();
+    
+    if (packetSize > 0)
+    {
+        // Read the UDP packet
+        int bytesReceived = _udp->read(_udpBuffer, sizeof(_udpBuffer));
+        
+        // Parse all bytes through the packet parser
+        for (int i = 0; i < bytesReceived; i++)
+        {
+            _bytesRead = packet.parse(_udpBuffer[i], true);
+            _status = packet.getStatus();
+            
+            // If we got a complete packet, return immediately
+            if (_status == PACKET_NEW_DATA)
+            {
+                return _bytesRead;
+            }
+            
+            // If there was an error, stop processing
+            if (_status < 0)
+            {
+                reset();
+                return 0;
+            }
+        }
+    }
+    else
+    {
+        // No data available - call parse with invalid flag to check for stale packets
+        _bytesRead = packet.parse(0, false);
+        _status = packet.getStatus();
+    }
+    
+    return 0;
+}
+
+void UDPTransfer::reset()
+{
+    packet.reset();
+    _bytesRead = 0;
+    _status = PACKET_NO_DATA;
+}
