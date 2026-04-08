c0d5e07a4f48b5e675f03dcd14f630459926e55e
Andy <aayre@esacademy.com>
2026-04-07 19:11:02 -0700

    Refactoring.

--- Firmware/TandemLevellerV2/src/AgGrade.cpp
+++ Firmware/TandemLevellerV2/src/AgGrade.cpp
@@ -6,10 +6,25 @@
 // constructor
 AgGrade::AgGrade
   (
-    UDPTransfer *_pUdpTransfer
+  void
   )
 {
-  this->pUdpTransfer = _pUdpTransfer;
+}
+
+// connects to AgGrade
+void AgGrade::Connect
+  (
+  unsigned int LocalPort,       // port that we listen on
+  IPAddress RemoteIPAddress,    // IP address of AgGrade
+  unsigned int RemotePort       // port that AgGrade is listening on
+  )
+{
+  // start UDP
+  Udp.begin(LocalPort);
+
+  // Initialize UDP Transfer for packet-based communication
+  UdpTransfer.begin(Udp);
+  UdpTransfer.setRemote(RemoteIPAddress, RemotePort);
 }
 
 // sends status value over UDP using packet framing
@@ -19,15 +34,45 @@ void AgGrade::SendStatus
   )
 {
   // Pack the PGN
-  pUdpTransfer->packet.txBuff[0] = (uint8_t)(pStatus->PGN & 0xFF);
-  pUdpTransfer->packet.txBuff[1] = (uint8_t)((pStatus->PGN >> 8) & 0xFF);
+  UdpTransfer.packet.txBuff[0] = (uint8_t)(pStatus->PGN & 0xFF);
+  UdpTransfer.packet.txBuff[1] = (uint8_t)((pStatus->PGN >> 8) & 0xFF);
 
   // Pack the data
   for (int b = 0; b < MAX_PGN_LEN; b++)
   {
-    pUdpTransfer->packet.txBuff[2 + b] = pStatus->Data[b];
+    UdpTransfer.packet.txBuff[2 + b] = pStatus->Data[b];
   }
 
   // Send the packet
-  pUdpTransfer->sendData(MAX_PGN_LEN + 2);
+  UdpTransfer.sendData(MAX_PGN_LEN + 2);
+}
+
+// checks if a command is waiting to be read
+// returns true if command waiting
+bool AgGrade::IsCommandAvailable
+  (
+  void  
+  )
+{
+  if (UdpTransfer.available() > 0) return true;
+
+  return false;
+}
+
+// gets a command from AgGrade
+pgnpacket_t AgGrade::GetCommand
+  (
+  void
+  )
+{
+  pgnpacket_t Command;
+
+  Command.PGN = (pgn_t)(((uint16_t)UdpTransfer.packet.rxBuff[1] << 8) | 
+                         UdpTransfer.packet.rxBuff[0]);
+  for (int b = 0; b < MAX_PGN_LEN; b++)
+  {
+    Command.Data[b] = UdpTransfer.packet.rxBuff[2 + b];
+  }
+
+  return Command;
 }
